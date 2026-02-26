5df7b0451ad7c4f2a254e9a5ba17392cdef3e0f2
personaive <nik.bachurin2704@mail.ru>
2026-02-26 20:54:52 +0500

    LW

---
+++ LW_1_2/Program.fs
@@ -0,0 +1,30 @@
+﻿open System
+
+let rec checkNaturalNumber () =
+    printfn "Введите натуральное число:"
+    let input = Console.ReadLine()
+    let (success, number) = Int32.TryParse input
+
+    if not success || number <= 0 then
+        printfn "Ошибка: введите натуральное число!"
+        checkNaturalNumber()
+
+    else
+        number
+
+let userNumber = 
+    checkNaturalNumber()
+
+let numberAsString = 
+    string userNumber
+
+let digitsList = 
+    [ for c in numberAsString -> int c - int '0' ]  // каждый символ превращается в цифру
+
+let evenDigits = 
+    [ for d in digitsList do
+        if d % 2 = 0 then
+            yield d ]
+
+let sumEvenDigits = List.sum evenDigits
+printfn "Сумма чётных цифр числа: %d" sumEvenDigits
